62a91dada7beaee5c5515d895f5b47ca5266bc56
Nickson Kipruto Rop <nickyrutto@gmail.com>
2024-01-01 17:59:48 +0300

    new models

--- patients/models.py
+++ patients/models.py
@@ -31,7 +31,7 @@ class Patient(models.Model):
     address = models.CharField(max_length=40)
     mobile = models.CharField(max_length=20, null=False)
     symptoms = models.CharField(max_length=100, null=True, blank=True)
-    assignedDoctorId = models.PositiveBigIntegerField(null=True)
+    assignedDoctorId = models.PositiveIntegerField(null=True)
     admitDate = models.DateField(auto_now=True)
     status = models.BooleanField(default=False)
 
@@ -44,3 +44,28 @@ class Patient(models.Model):
         return self.user.id
     def __str__(self):
         return self.user.first_name+" ("+self.symptoms+")"
+    
+    class Appointment(models.Model):
+        patientId = models.PositiveIntegerField(null=True)
+        doctorId = models.PositiveIntegerField(null=True)
+        patientName = models.CharField(max_length=40, null=True)
+        doctorName = models.CharField(max_length=40, null=True)
+        appointementDate = models.DateField(auto_now=True)
+        description=models.TextField(max_length=500)
+        status=models.BooleanField(default=False)
+
+    class PatientDischargeDetails(models.Model):
+        patientId = models.PositiveIntegerField(null=True)
+        patientName = models.CharField(max_length=40)
+        assignedDoctorName = models.PositiveIntegerField(null=True)
+        address = models.CharField(max_length=40)
+        mobile = models.CharField(max_length=20, null=False)
+        symptoms = models.CharField(max_length=100, null=True, blank=True)
+        admitDate = models.DateField(null=False)
+        releaseDate = models.DateField(null=False)
+        daySpent = models.PositiveIntegerField(null=False)
+        roomCharge = models.PositiveIntegerField(null=False)
+        medicineCost = models.PositiveIntegerField(null=False)
+        doctorFee = models.PositiveIntegerField(null=False)
+        otherCharge= models.PositiveIntegerField(null=False)
+        total = models.PositiveIntegerField(null=False)
